0e2ee6077e2a00be554f027975a52cc4eec9d672
iberatkaya <beratkaya1998@gmail.com>
2019-11-28 23:39:45 +0300

    Updated project

--- projectgroupapp/AppNav.js
+++ projectgroupapp/AppNav.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, ScrollView, TouchableOpacity, Linking, Dimensions} from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import {createDrawerNavigator} from 'react-navigation-drawer';
 import HomeScreen from './screens/Home';
 import ExchangeScreen from './screens/Exchanges';
+
+const height = Dimensions.get("window").height;
 
 const Stack = createStackNavigator({
     Home: HomeScreen,
@@ -13,14 +15,22 @@ const Stack = createStackNavigator({
 
 const Drawer = createDrawerNavigator({Home: Stack},
     {
-        /*contentComponent:(props) => (
-            <View>
-                <Text>hey</Text>
-            </View>
-        )*/
+        contentComponent:(props) => (
+            <ScrollView>
+                <View style={{ marginBottom: 4, height: height * 0.25, alignItems: 'center', justifyContent: 'center', backgroundColor: 'red' }}>
+                </View>
+                <TouchableOpacity
+                    onPress={() => { Linking.openURL("mailto:ibraberatkaya@gmail.com?subject=Feedback"); }}
+                >
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                        <Text style={{ fontSize: 14, paddingLeft: 32, color: 'black' }}>Feedback</Text>
+                    </View>
+                </TouchableOpacity>
+            </ScrollView>
+        )
     }
     );
 
 
 
-export default createAppContainer(Stack);
+export default createAppContainer(Drawer);
--- projectgroupapp/screens/Home.js
+++ projectgroupapp/screens/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, View, Text, FlatList } from 'react-native';
+import { ScrollView, View, Text, FlatList, Picker } from 'react-native';
 
 
 export default class Home extends React.Component {
@@ -10,7 +10,7 @@ export default class Home extends React.Component {
         super(props);
         this.state = {
             base: 'EUR',
-            rates: {}
+            rates: []
         }
     }
 
@@ -25,8 +25,8 @@ export default class Home extends React.Component {
         }
     );
 
-    async componentDidMount() {    //Component mountlandıktan sonra bu fonksiyonu execute et
-        let req = await fetch('https://api.exchangeratesapi.io/latest');    //API'a istek at ve geri dönüş al
+    getRates = async () => {
+        let req = await fetch('https://api.exchangeratesapi.io/latest?base=' + this.state.base);    //API'a istek at ve geri dönüş al
         let jsObj = await req.json();     //Fetch'in döndürüğü response'daki JSON objesini bul, JS objesine çevir ve jsObj desğişkenine ata
         /*
           JSON.parse() vs .json()
@@ -47,10 +47,38 @@ export default class Home extends React.Component {
         });
     }
 
+    async componentDidMount() {    //Component mountlandıktan sonra bu fonksiyonu execute et
+        await this.getRates();    
+    }
+
+    labels = () => {
+        let i = 0;
+        if(this.state.rates.length == 0)
+            return <Picker.Item label="EUR" value={this.state.base} key={0}/>;
+        return this.state.rates.map(item => {
+            return(<Picker.Item label={item.name} value={item.name} color="black" key={i++}/>)
+        });
+    }
+
     render() {   //Ekranda görülecek her şey render'da gerçekleşir
+        let a = this.labels();
         return (
             <ScrollView>
-                <Text style = {{fontSize: 20}}>Base: {this.state.base}</Text>
+                <Picker
+                    mode="dropdown"
+                    selectedValue={this.state.base}
+                    style={{height: 50, width: 150}}
+                    onValueChange={(value, itemIndex) => {
+                        this.setState({base: value}, async () => {
+                            await this.getRates();
+                        })
+                        
+                    }
+                }>
+                    {
+                        a
+                    }
+                </Picker>
                 <FlatList
                     data={this.state.rates}
                     keyExtractor={(item, index) => index.toString()}
